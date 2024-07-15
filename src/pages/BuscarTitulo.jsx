5e392a98c112a6621efd9fd172d9a55f7fc9464f
pricbnll <pricbnll@gmail.com>
2024-07-15 15:55:48 -0300

    feat: Upadate exercicio 2 com mais um exempli - math.randow

--- src/pages/BuscarTitulo.jsx
+++ src/pages/BuscarTitulo.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react"
 
 export function BuscarTitulo() {
 
-  
     const [titulo, setTitulo] = useState(null)
 
     async function getData() {
@@ -20,9 +19,43 @@ export function BuscarTitulo() {
     return (
         <div className="container mt-4">
            <h1>Noticias: </h1>
-
-            <p>{titulo}</p>
-            
+         <p>{titulo}</p>
         </div>
     )
 }
+
+// export function BuscarTitulo() {
+//     const [data, setData] = useState([]);
+//     const [randomTitles, setRandomTitles] = useState([]);
+
+//     async function getData() {
+//         const response = await fetch('https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release');
+//         const data = await response.json();
+//         setData(data.items); // items should be the array from the data
+//         selectRandomTitles(data.items);
+//     }
+
+//     function selectRandomTitles(items) {
+//         const titles = [];
+//         for (let i = 0; i < 1; i++) { // change 5 to any number of titles you want to display
+//             const randomIndex = Math.floor(Math.random() * items.length);
+//             titles.push(items[randomIndex].titulo);
+//         }
+//         setRandomTitles(titles);
+//     }
+
+//     useEffect(() => {
+//         getData();
+//     }, []);
+
+//     return (
+//         <div className="container mt-4">
+//             <h1>Notícias: </h1>
+
+                //retirar a arrow function daqui e colocar fora do return
+//             {randomTitles.map((titulo, index) => (
+//                 <p key={index}>{titulo}</p>
+//             ))}
+//         </div>
+//     );
+// }
